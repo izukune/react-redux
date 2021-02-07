a4731c90ee0644f513a0d9b9c0265d7e1b07de53
izukune <izuizuizu1009@gmail.com>
2021-02-08 00:49:13 +0900

    add functional componets

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import React from 'react';
 function App() {
   return (
     <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={()=>{console.log("I am changed")}} />
+      <Cat />
+      <Cat />
     </React.Fragment>
   );
+}
+
+const Cat = ()=>{
+  return <div>Meow!</div>
 }
 
 export default App;
