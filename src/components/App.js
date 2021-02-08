544be8e6e6218e454e5cf89660771b41a7c905ae
izukune <izuizuizu1009@gmail.com>
2021-02-08 18:25:20 +0900

    change form of mapDispatchToProps

--- src/components/App.js
+++ src/components/App.js
@@ -20,9 +20,6 @@ const mapStateToProps = (state) => ({
   value: state.count.value,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  increment: () => dispatch(increment()),
-  decrement: () => dispatch(decrement()),
-});
+const mapDispatchToProps = { increment, decrement };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
