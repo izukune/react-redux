cc43c5fbc54195765def065c9472e32799c1c3e6
izukune <izuizuizu1009@gmail.com>
2021-02-08 15:51:08 +0900

    fix worning for export default

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
-import { Provider } from 'react-redux'
-import './index.css';
-import App from './components/App';
-import reducer from reducers
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./components/App";
+import reducer from "./reducers";
+import reportWebVitals from "./reportWebVitals";
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <Provider store = {store}>
+  <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
