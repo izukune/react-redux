755300aa250aca508b6224ba3d3e82143dc0fcb9
izukune <izuizuizu1009@gmail.com>
2021-02-08 14:28:17 +0900

    learn reducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-const App = ()=>(<Counter></Counter>)
+const App = () => <Counter></Counter>;
 
 class Counter extends Component {
-  constructor(props){
-    super(props)
-    this.state = {count: 0}
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
   }
 
-  hundleUp = ()=>{ this.setState({count: this.state.count + 1})}
-  hundleDown = ()=>{ this.setState({count: this.state.count - 1})}
+  hundleUp = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  hundleDown = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
 
-  render(){
-    return(
+  render() {
+    return (
       <React.Fragment>
         <div>count: {this.state.count}</div>
         <button onClick={this.hundleUp}>+1</button>
         <button onClick={this.hundleDown}>-1</button>
       </React.Fragment>
-    )
+    );
   }
 }
 
